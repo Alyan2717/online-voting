d31100c8a7bb7f99d59f4d3993c0baa031a9c07c
Alyan2717 <outsource.developer1@decibel360cloud.com>
2025-03-08 03:40:35 +0500

    test commit

--- src/main/java/com/web_app/online_voting/model/district.java
+++ src/main/java/com/web_app/online_voting/model/district.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "district")
 public class district {
+
     @Id
     public String id;
     public String districtCode;
